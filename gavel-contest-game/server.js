120e299f1a8c51e213281e08fe07fd7db9165f69
Bongor <64134832+Bongor@users.noreply.github.com>
2022-03-07 15:56:15 +0100

    Server handles requests

--- gavel-contest-game/server.js
+++ gavel-contest-game/server.js
@@ -9,62 +9,65 @@ const formidable = require("formidable");
 const fs = require("fs");
 const lookup = require("mime-types").lookup
 
-const server = http.createServer(function(req, res) {
+const server = http.createServer(function (req, res) {
 
-    //res.setHeader("Content-type", "application/json")
+    res.setHeader("Content-type", "application/json")
     res.setHeader("Access-Control-Allow-Origin", "*")
 
-    let parsedURL = url.parse(req.url, true)
-    let path = parsedURL.path.replace(/^\/+|\/+$/g, "")
-    if (path == "") {
-        path = "test.html"
-    }
-    path = path.replace(/\//g, "\\")
-    let file = __dirname + "\\"+ path
-    fs.readFile(file, function(err, content) {
-        if (err) {
-            console.log(`File not found ${file}`)
-            res.writeHead(404)
-            res.end()
-        } else if (path == "config.js") {
-            res.setHeader("X-Content-Type-Options", "nosniff")
-            res.writeHead(200, {"Content-type": "application/javascript"})
-        } 
-        
-        else {
-            console.log(path)
-            let mime = lookup(path)
-            res.setHeader("X-Content-Type-Options", "nosniff")
-            res.writeHead(200, {"Content-type": mime})
+    const buffers = [];
+
+    if (req.method.toLowerCase() == 'post') {
+        var body = ""
+        req.on("data", function(chunk) {
+            body += chunk
+        })
+        req.on("end", function() {
+            res.writeHead(200, {"Content-Type": "text/html"})
+            console.log(body)
+            res.end(body)
+        })
+    } else if (req.method.toLowerCase() == 'get') {
+        let parsedURL = url.parse(req.url, true)
+        let path = parsedURL.path.replace(/^\/+|\/+$/g, "")
+        if (path == "") {
+            path = "test.html"
         }
-        res.end(content)
-    })
+        path = path.replace(/\//g, "\\")
+        let file = __dirname + "\\" + path
+        fs.readFile(file, function (err, content) {
+            if (err) {
+                console.log(`File not found ${file}`)
+                res.writeHead(404)
+                res.end()
+            } else if (path == "config.js") {
+                res.setHeader("X-Content-Type-Options", "nosniff")
+                res.writeHead(200, { "Content-type": "application/javascript" })
+            }
 
-    
+            else {
+                let mime = lookup(path)
+                res.setHeader("X-Content-Type-Options", "nosniff")
+                res.writeHead(200, { "Content-type": mime })
+            }
+            res.end(content)
+        })
+    }
 
-    // if (req.method.toLowerCase() == 'post') {
-    //     let form = new formidable.IncomingForm()
-    //     form.parse(req, function(err, fields, files) {
-    //         if(err) {
-    //             console.log(err.message)
-    //         }
-    //         console.log(fields.game)
-    //         res.end(fields.game)
-    //     })
-    // } else if (req.method.toLowerCase() == "get") {
-    //     res.writeHead(200, "OK", {"Content-Type": "text/plain"})
-    //     res.end(req.url)
-    // }
+
+
+
+
+
 });
 
-server.listen(8000, function() {
-  console.log("Listening on port 8000");
+server.listen(8000, function () {
+    console.log("Listening on port 8000");
 });
 
 // var http = require('http');
 // var fs = require('fs');
 
-// const PORT=8000; 
+// const PORT=8000;
 
 // fs.readFile('./test.html', function (err, html) {
 
