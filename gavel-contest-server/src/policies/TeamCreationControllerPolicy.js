b026b9806daed831d8aa618c86bc358159ac164b
Bongor <64134832+Bongor@users.noreply.github.com>
2022-03-22 17:48:21 +0100

    Added restrictions on team name

--- gavel-contest-server/src/policies/TeamCreationControllerPolicy.js
+++ gavel-contest-server/src/policies/TeamCreationControllerPolicy.js
@@ -2,13 +2,13 @@ const Joi = require('joi')
 
 module.exports = {
     create(req, res, next) {
-        const schema = Joi.string().min(3).max(30).required()
+        const schema = Joi.string().min(3).max(30).required().pattern(new RegExp('^[a-zA-Z][a-zA-Z0-9 ]+$'))
 
         const {error, value} = schema.validate(req.body.name)
 
         if (error) {
             res.status(400).send({
-                error: "Please provide a team name between 3 and 30 characters"
+                error: "Please provide a team name between 3 and 30 characters using only alphanumerical characters starting with an alphabetical character"
             })
         } else {
             next()
