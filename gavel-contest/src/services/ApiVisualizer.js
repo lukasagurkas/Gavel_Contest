335e30fae45d10bf0c292a0eb3b8b324b80716b4
lukasagurkas <lukasagurkas@gmail.com>
2022-04-06 13:18:14 +0200

    Update ApiVisualizer.js

--- gavel-contest/src/services/ApiVisualizer.js
+++ gavel-contest/src/services/ApiVisualizer.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: `/contest/visualiser/`
+        baseURL: `/contest/visualizer/`
         //baseURL: `http://localhost:8000/` TODO
     })
 }
