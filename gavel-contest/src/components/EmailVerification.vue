ce1e31881d1f791508e32b76564822a4428b49fd
Bongor <64134832+Bongor@users.noreply.github.com>
2022-05-03 08:14:39 +0200

    Updated email verification css

--- gavel-contest/src/components/EmailVerification.vue
+++ gavel-contest/src/components/EmailVerification.vue
@@ -1,43 +1,47 @@
 <template>
-  <div>
-    <div class="verificationResponse" />
-    <div>
-      <p><b>Current email:</b> {{ email }}</p>
-    </div>
-    <p>Please verify your email</p>
-    <div>
-      <button class="resend-button" @click="resendVerificationEmail">
-        Resend
+  <div class="container">
+    <div class="header">
+      <button style="margin-top: 20px" class="button" @click="logOut">
+        Log out
       </button>
-      <button
-        style="margin-left: 20px"
-        class="continue-button"
-        @click="emailVerified"
-      >
-        Continue
-      </button>
-      <div class="error" />
     </div>
-    <div style="margin-top: 20px">
-      <input
-        name="new-email"
-        type="email"
-        class="text"
-        placeholder="Enter new email"
-      />
-      <button
-        style="margin-left: 5px"
-        class="setNewEmail-button"
-        @click="setNewEmail"
-      >
-        Set new email address
-      </button>
-      <div class="errorSettingNewEmail" />
+    <div class="content">
+      <div class="verificationResponse" />
+      <div>
+        <p><b>Current email:</b> {{ email }}</p>
+      </div>
+      <p>Please verify your email</p>
+      <div>
+        <button class="button" @click="resendVerificationEmail">
+          Resend
+        </button>
+        <button
+          style="margin-left: 20px"
+          class="button"
+          @click="emailVerified"
+        >
+          Continue
+        </button>
+        <div class="error" />
+      </div>
+      <div style="margin-top: 20px">
+        <input
+          name="new-email"
+          type="email"
+          class="text"
+          placeholder="Enter new email"
+        />
+        <button
+          style="margin-left: 5px"
+          class="button"
+          @click="setNewEmail"
+        >
+          Set new email address
+        </button>
+        <div class="errorSettingNewEmail" />
+      </div>
+      <div class="verificationError" />
     </div>
-    <button style="margin-top: 20px" class="logout-button" @click="logOut">
-      Log out
-    </button>
-    <div class="verificationError" />
   </div>
 </template>
 
@@ -141,19 +145,17 @@ export default {
   color: red;
 }
 
-.logout-button,
-.resend-button,
-.continue-button,
-.setNewEmail-button {
+.button {
   padding: 0.5em 1em;
-  background-color: #dee2e6;
-  color: #212529;
-  border: 0.1px solid #212529;
+  background-color: #4BB052;
+  color: #F2FFED;
+  border: 0.1px solid #F2FFED;
   border-radius: 5px;
   font-weight: bold;
   font-size: 16px;
   text-transform: uppercase;
   cursor: pointer;
+  font-family: 'Fira Sans';
 }
 
 .text {
@@ -165,5 +167,33 @@ export default {
   font-weight: bold;
   font-size: 16px;
   cursor: pointer;
+  font-family: 'Fira Sans';
+}
+
+.container {
+  font-family: 'Fira Sans';
+}
+
+.header {
+  width: 100%;
+  margin: 1rem;
+  height: 50px;
+}
+
+.header .button {
+  margin-right: 50px;
+  float: right;
+}
+
+.content {
+  width: 70%;
+  height: 350px;
+  padding: 1rem;
+  padding-top: 100px;
+  margin-left: 15%;
+  text-align: center;
+  background-color: #ffffff;
+  border-radius: 5px;
+  box-shadow: 2px 4px 8px rgba(0, 0, 0, 0.2);
 }
 </style>
